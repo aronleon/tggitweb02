26247b2c48394b2a84a0e123776eb84171ffdefb
aronleon <aronleonsalamon@gmail.com>
2024-10-13 15:08:42 +0200

    bbgfhgf

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ async function getTabsFromLink() {
         formData.append('dt', dt);
 
         try {
-            const response = await fetch('https://9821-37-46-167-35.ngrok-free.app/convertLink', {
+            const response = await fetch('https://37.46.167.35:8000/convertLink/', {
                 method: 'POST',
                 body: formData
             });
